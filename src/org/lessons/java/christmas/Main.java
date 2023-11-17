4d8f2bb70d1f901c77589c41ec927642381e11c4
SimoneVilardo <simovila95@gmail.com>
2023-11-17 12:57:40 +0100

    esercizio bonus completato

--- src/org/lessons/java/christmas/Main.java
+++ src/org/lessons/java/christmas/Main.java
@@ -2,25 +2,62 @@ package org.lessons.java.christmas;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+
+import org.lessons.java.christmas.pojo.Regalo;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		List<String> regali = new ArrayList<>();
+//		List<String> regali = new ArrayList<>();
+//		
+//		Scanner in = new Scanner(System.in);
+//
+//		while (true) {
+//			
+//			System.out.print("Inserisci il nome del regalo: ");
+//            String regalo = in.nextLine();
+//            
+//            regali.add(regalo);
+//            
+//            System.out.println("Lunghezza della lista: " + regali.size());
+//            
+//            System.out.println("Vuoi inserire un altro regalo? ");
+//            String altroRegalo = in.nextLine();
+//            
+//            if (altroRegalo.equals("no")) {
+//				break;
+//			}
+//		}
+//		
+//		
+//        Collections.sort(regali);
+//
+//        
+//        System.out.println("Lista dei regali:");
+//        for (String regalo : regali) {
+//            System.out.println(regalo);
+//        }
+		
+		List<Regalo> listaRegali = new ArrayList<>();
 		
 		Scanner in = new Scanner(System.in);
 
 		while (true) {
 			
 			System.out.print("Inserisci il nome del regalo: ");
-            String regalo = in.nextLine();
+            String nomeRegalo = in.nextLine();
             
-            regali.add(regalo);
+            System.out.print("A chi è destinato il regalo? ");
+            String destinatarioRegalo = in.nextLine();
             
-            System.out.println("Lunghezza della lista: " + regali.size());
+            Regalo regalo = new Regalo( nomeRegalo, destinatarioRegalo );
+            listaRegali.add(regalo);
+            
+            System.out.println("Lunghezza della lista: " + listaRegali.size());
             
             System.out.println("Vuoi inserire un altro regalo? ");
             String altroRegalo = in.nextLine();
@@ -30,13 +67,19 @@ public class Main {
 			}
 		}
 		
-		// Ordina la lista dei regali
-        Collections.sort(regali);
+		 System.out.print("Vuoi ordinare la lista per nome o per destinatario? ");
+	     String modoOrdinamento = in.nextLine();
+		
+	     if (modoOrdinamento.equals("nome")) {
+	            Collections.sort(listaRegali, Comparator.comparing(regalo -> regalo.getNome()));
+	        } else if (modoOrdinamento.equals("destinatario")) {
+	            Collections.sort(listaRegali, Comparator.comparing(regalo -> regalo.getDestinatario()));
+	        }
 
-        // Stampa la lista dei regali
+        
         System.out.println("Lista dei regali:");
-        for (String regalo : regali) {
-            System.out.println(regalo);
+        for (Regalo regalo : listaRegali) {
+            System.out.println("Nome: " + regalo.getNome() + ", Destinatario: " + regalo.getDestinatario());
         }
 	}
 
--- src/org/lessons/java/christmas/pojo/Regalo.java
+++ src/org/lessons/java/christmas/pojo/Regalo.java
@@ -1,5 +1,31 @@
 package org.lessons.java.christmas.pojo;
 
 public class Regalo {
+	
+	String nome;
+	String destinatario;
+	
+	public Regalo(String nome, String destinatario) {
+		
+		
+		setNome(nome);
+		setDestinatario(destinatario);
+		
+	}
 
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDestinatario() {
+		return destinatario;
+	}
+
+	public void setDestinatario(String destinatario) {
+		this.destinatario = destinatario;
+	}
 }
